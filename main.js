7ee53f89788fe8453a2470b80fdb99e0f065e211
BlackCult666 <52260718+BlackCult666@users.noreply.github.com>
2024-04-19 23:19:50 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,51 @@
+const circleType = new CircleType(document.querySelector(".rotating__section")).radius(50);
+
+const sectionBalls = document.querySelectorAll('.section__ball');
+
+
+sectionBalls.forEach(ball => {
+    
+    ball.addEventListener('mouseenter', () => {
+        const hoveredId = ball.id;
+        sectionBalls.forEach(otherBall => {
+            if (otherBall.id !== hoveredId) {
+                if (otherBall.id === 'photo' || otherBall.id === 'about') {
+                    otherBall.classList.add('hideToLeft');
+                } else {
+                    otherBall.classList.add('hideToRight');
+                }
+            }
+        });
+    });
+
+    ball.addEventListener('mouseleave', () => {
+        sectionBalls.forEach(otherBall => {
+            otherBall.classList.remove('hideToRight');
+            otherBall.classList.remove('hideToLeft');
+        });
+    });
+});
+
+/*sectionBalls.forEach(ball => {
+    ball.addEventListener('mouseover', () => {
+        // Nascondi tutte le altre sezioni
+        sectionBalls.forEach(otherBall => {
+            if (otherBall !== ball) {
+                otherBall.style.display = 'none';
+            }
+        });
+        // Visualizza la "cosa" desiderata, sostituisci "cosa" con il tuo elemento
+        // Esempio:
+        // document.getElementById('elemento-da-mostrare').style.display = 'block';
+    });
+
+    ball.addEventListener('mouseout', () => {
+        // Mostra tutte le altre sezioni quando esce l'hover
+        sectionBalls.forEach(otherBall => {
+            otherBall.style.display = 'flex';
+        });
+        // Nascondi la "cosa" quando esce l'hover, sostituisci "cosa" con il tuo elemento
+        // Esempio:
+        // document.getElementById('elemento-da-mostrare').style.display = 'none';
+    });
+});*/
