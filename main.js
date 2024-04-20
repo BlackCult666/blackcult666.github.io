a52f442485a2badf555a143905d65cb8bc9570e5
BlackCult666 <52260718+BlackCult666@users.noreply.github.com>
2024-04-20 14:47:15 +0200

    fix

--- main.js
+++ main.js
@@ -3,36 +3,39 @@ const circleType = new CircleType(document.querySelector(".rotated__section")).r
 const draggableCircle = document.querySelector(".draggable__circle");
 let offsetX, offsetY;
 
-const move = (e) => {
-    let posX, posY;
-    if (e.type === "touchmove") {
-        posX = e.touches[0].clientX;
-        posY = e.touches[0].clientY;
-    } else {
-        posX = e.clientX;
-        posY = e.clientY;
-    }
-    draggableCircle.style.left = `${posX - offsetX}px`;
-    draggableCircle.style.top = `${posY - offsetY}px`;
-};
-
 draggableCircle.addEventListener("mousedown", startDrag);
 draggableCircle.addEventListener("touchstart", startDrag);
 
 function startDrag(e) {
-    offsetX = e.clientX - draggableCircle.offsetLeft;
-    offsetY = e.clientY - draggableCircle.offsetTop;
+    e.preventDefault(); // Evita il comportamento predefinito del browser
+
+    const event = e.type === "touchstart" ? e.touches[0] : e;
+
+    offsetX = event.clientX - draggableCircle.offsetLeft;
+    offsetY = event.clientY - draggableCircle.offsetTop;
 
     document.addEventListener("mousemove", move);
+    document.addEventListener("touchmove", move, { passive: false });
     document.addEventListener("mouseup", endDrag);
-    document.addEventListener("touchmove", move);
     document.addEventListener("touchend", endDrag);
+}
+
+function move(e) {
+    e.preventDefault(); // Evita il comportamento predefinito del browser
+
+    const event = e.type === "touchmove" ? e.touches[0] : e;
+
+    const posX = event.clientX - offsetX;
+    const posY = event.clientY - offsetY;
+
+    draggableCircle.style.left = `${posX}px`;
+    draggableCircle.style.top = `${posY}px`;
 }
 
 function endDrag() {
     document.removeEventListener("mousemove", move);
-    document.removeEventListener("mouseup", endDrag);
     document.removeEventListener("touchmove", move);
+    document.removeEventListener("mouseup", endDrag);
     document.removeEventListener("touchend", endDrag);
 }
 
