689077c6a814997b7f75bfe477e76f8a8d24fb29
BlackCult666 <52260718+BlackCult666@users.noreply.github.com>
2024-04-20 14:23:22 +0200

    Changes

--- main.js
+++ main.js
@@ -1,27 +1,42 @@
-const circleType = new CircleType(document.querySelector(".rotating__section")).radius(50);
+const circleType = new CircleType(document.querySelector(".rotated__section")).radius(50);
+const draggableCircle = document.querySelector(".draggable__circle");
+let offsetX, offsetY;
+
+const move = (e) => {
+    draggableCircle.style.left = `${e.clientX - offsetX}px`;
+    draggableCircle.style.top = `${e.clientY - offsetY}px`;
+}
+
+draggableCircle.addEventListener("mousedown", (e) => {
+    offsetX = e.clientX - draggableCircle.offsetLeft;
+    offsetY = e.clientY - draggableCircle.offsetTop;
+
+    document.addEventListener("mousemove", move);
+});
+
+document.addEventListener("mouseup", () => {
+    document.removeEventListener("mousemove", move);
+});
+
+
 
 const sectionBalls = document.querySelectorAll('.section__ball');
 
 
 sectionBalls.forEach(ball => {
 
-    let isClicked = false;
-    
     const handleMouseEnter = () => {
-        if(!isClicked) {
-            isClicked = true;
             
-            const hoveredId = ball.id;
-            sectionBalls.forEach(otherBall => {
-                if (otherBall.id !== hoveredId) {
-                    if (otherBall.id === 'photo' || otherBall.id === 'about') {
-                        otherBall.classList.add('hideToLeft');
-                    } else {
-                        otherBall.classList.add('hideToRight');
-                    }
+        const hoveredId = ball.id;
+        sectionBalls.forEach(otherBall => {
+            if (otherBall.id !== hoveredId) {
+                if (otherBall.id === 'photo' || otherBall.id === 'about') {
+                    otherBall.classList.add('hideToLeft');
+                } else {
+                    otherBall.classList.add('hideToRight');
                 }
-            });
-        }
+            }
+        });
     };
 
     const handleMouseExit = () => {
